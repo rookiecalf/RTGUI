a621e863ff6faea1a9c6488f8555f681f302f4ae
Bernard Xiong <bernard.xiong@gmal.com>
2014-05-06 11:35:32 +0800

    Add SDL2_gfx copyright

--- components/rtgui/common/dc_blend.c
+++ components/rtgui/common/dc_blend.c
@@ -20,6 +20,8 @@
  * Change Logs:
  * Date           Author       Notes
  * 2013-10-04     Bernard      porting SDL software render to RT-Thread GUI
+ * 2014-05-06     Bernard      Add the SDL2_gfx copyright for aa_ecllipse/
+ *                             thick_line etc.
  */
 
 /*
@@ -42,6 +44,33 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
+
+/*
+  SDL2_gfx: graphics primitives for SDL
+  Copyright (C) 2012 Andreas Schiffler
+  
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  
+  1. The origin of this software must not be misrepresented; you must not
+  claim that you wrote the original software. If you use this software
+  in a product, an acknowledgment in the product documentation would be
+  appreciated but is not required.
+  
+  2. Altered source versions must be plainly marked as such, and must not be
+  misrepresented as being the original software.
+
+  3. This notice may not be removed or altered from any source
+  distribution.
+
+  Andreas Schiffler -- aschiffler at ferzkopp dot net
+*/
+
 #include <rtgui/dc.h>
 #include <rtgui/dc_draw.h>
 #include <rtgui/color.h>
